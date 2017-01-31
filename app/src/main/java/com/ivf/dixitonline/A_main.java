a14ec65ceb7463375f9ffdf292a134b826544c79
dnsrl1753 <dnsrl1753@gmail.com>
2017-01-31 22:33:44 +0900

    Initial commit

---
+++ app/src/main/java/com/ivf/dixitonline/A_main.java
@@ -0,0 +1,13 @@
+package com.ivf.dixitonline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class A_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
